ec14e127e64379ddb31b447e52806c99690ee6a2
fofr <fofr@users.noreply.github.com>
2024-07-08 09:46:38 +0100

    Initial commit

---
+++ predict.py
@@ -0,0 +1,123 @@
+import os
+import shutil
+import tarfile
+import zipfile
+import mimetypes
+from typing import List
+from cog import BasePredictor, Input, Path
+from comfyui import ComfyUI
+from weights_downloader import WeightsDownloader
+from cog_model_helpers import optimise_images
+from config import config
+
+
+os.environ["DOWNLOAD_LATEST_WEIGHTS_MANIFEST"] = "true"
+mimetypes.add_type("image/webp", ".webp")
+OUTPUT_DIR = "/tmp/outputs"
+INPUT_DIR = "/tmp/inputs"
+COMFYUI_TEMP_OUTPUT_DIR = "ComfyUI/temp"
+ALL_DIRECTORIES = [OUTPUT_DIR, INPUT_DIR, COMFYUI_TEMP_OUTPUT_DIR]
+
+with open("examples/api_workflows/sd15_txt2img.json", "r") as file:
+    EXAMPLE_WORKFLOW_JSON = file.read()
+
+
+class Predictor(BasePredictor):
+    def setup(self, weights: str):
+        if bool(weights):
+            self.handle_user_weights(weights)
+
+        self.comfyUI = ComfyUI("127.0.0.1:8188")
+        self.comfyUI.start_server(OUTPUT_DIR, INPUT_DIR)
+
+    def handle_user_weights(self, weights: str):
+        print(f"Downloading user weights from: {weights}")
+        WeightsDownloader.download("weights.tar", weights, config["USER_WEIGHTS_PATH"])
+        for item in os.listdir(config["USER_WEIGHTS_PATH"]):
+            source = os.path.join(config["USER_WEIGHTS_PATH"], item)
+            destination = os.path.join(config["MODELS_PATH"], item)
+            if os.path.isdir(source):
+                if not os.path.exists(destination):
+                    print(f"Moving {source} to {destination}")
+                    shutil.move(source, destination)
+                else:
+                    for root, _, files in os.walk(source):
+                        for file in files:
+                            if not os.path.exists(os.path.join(destination, file)):
+                                print(
+                                    f"Moving {os.path.join(root, file)} to {destination}"
+                                )
+                                shutil.move(os.path.join(root, file), destination)
+                            else:
+                                print(
+                                    f"Skipping {file} because it already exists in {destination}"
+                                )
+
+    def handle_input_file(self, input_file: Path):
+        file_extension = os.path.splitext(input_file)[1].lower()
+        if file_extension == ".tar":
+            with tarfile.open(input_file, "r") as tar:
+                tar.extractall(INPUT_DIR)
+        elif file_extension == ".zip":
+            with zipfile.ZipFile(input_file, "r") as zip_ref:
+                zip_ref.extractall(INPUT_DIR)
+        elif file_extension in [".jpg", ".jpeg", ".png", ".webp"]:
+            shutil.copy(input_file, os.path.join(INPUT_DIR, f"input{file_extension}"))
+        else:
+            raise ValueError(f"Unsupported file type: {file_extension}")
+
+        print("====================================")
+        print(f"Inputs uploaded to {INPUT_DIR}:")
+        self.comfyUI.get_files(INPUT_DIR)
+        print("====================================")
+
+    def predict(
+        self,
+        workflow_json: str = Input(
+            description="Your ComfyUI workflow as JSON. You must use the API version of your workflow. Get it from ComfyUI using ‘Save (API format)’. Instructions here: https://github.com/fofr/cog-comfyui",
+            default="",
+        ),
+        input_file: Path = Input(
+            description="Input image, tar or zip file. Read guidance on workflows and input files here: https://github.com/fofr/cog-comfyui. Alternatively, you can replace inputs with URLs in your JSON workflow and the model will download them.",
+            default=None,
+        ),
+        return_temp_files: bool = Input(
+            description="Return any temporary files, such as preprocessed controlnet images. Useful for debugging.",
+            default=False,
+        ),
+        output_format: str = optimise_images.predict_output_format(),
+        output_quality: int = optimise_images.predict_output_quality(),
+        randomise_seeds: bool = Input(
+            description="Automatically randomise seeds (seed, noise_seed, rand_seed)",
+            default=True,
+        ),
+        force_reset_cache: bool = Input(
+            description="Force reset the ComfyUI cache before running the workflow. Useful for debugging.",
+            default=False,
+        ),
+    ) -> List[Path]:
+        """Run a single prediction on the model"""
+        self.comfyUI.cleanup(ALL_DIRECTORIES)
+
+        if input_file:
+            self.handle_input_file(input_file)
+
+        wf = self.comfyUI.load_workflow(workflow_json or EXAMPLE_WORKFLOW_JSON)
+
+        self.comfyUI.connect()
+
+        if force_reset_cache or not randomise_seeds:
+            self.comfyUI.reset_execution_cache()
+
+        if randomise_seeds:
+            self.comfyUI.randomise_seeds(wf)
+
+        self.comfyUI.run_workflow(wf)
+
+        output_directories = [OUTPUT_DIR]
+        if return_temp_files:
+            output_directories.append(COMFYUI_TEMP_OUTPUT_DIR)
+
+        return optimise_images.optimise_image_files(
+            output_format, output_quality, self.comfyUI.get_files(output_directories)
+        )
---
+++ weights_downloader.py
@@ -0,0 +1,83 @@
+import subprocess
+import time
+import os
+from weights_manifest import WeightsManifest
+
+
+class WeightsDownloader:
+    supported_filetypes = [
+        ".ckpt",
+        ".safetensors",
+        ".pt",
+        ".pth",
+        ".bin",
+        ".onnx",
+        ".torchscript",
+    ]
+
+    def __init__(self):
+        self.weights_manifest = WeightsManifest()
+        self.weights_map = self.weights_manifest.weights_map
+
+    def get_weights_by_type(self, type):
+        return self.weights_manifest.get_weights_by_type(type)
+
+    def download_weights(self, weight_str):
+        if weight_str in self.weights_map:
+            if self.weights_manifest.is_non_commercial_only(weight_str):
+                print(
+                    f"⚠️  {weight_str} is for non-commercial use only. Unless you have obtained a commercial license.\nDetails: https://github.com/fofr/cog-comfyui/blob/main/weights_licenses.md"
+                )
+
+            if isinstance(self.weights_map[weight_str], list):
+                for weight in self.weights_map[weight_str]:
+                    self.download_if_not_exists(
+                        weight_str, weight["url"], weight["dest"]
+                    )
+            else:
+                self.download_if_not_exists(
+                    weight_str,
+                    self.weights_map[weight_str]["url"],
+                    self.weights_map[weight_str]["dest"],
+                )
+        else:
+            raise ValueError(
+                f"{weight_str} unavailable. View the list of available weights: https://github.com/fofr/cog-comfyui/blob/main/supported_weights.md"
+            )
+
+    def check_if_file_exists(self, weight_str, dest):
+        if dest.endswith(weight_str):
+            path_string = dest
+        else:
+            path_string = os.path.join(dest, weight_str)
+        return os.path.exists(path_string)
+
+    def download_if_not_exists(self, weight_str, url, dest):
+        if self.check_if_file_exists(weight_str, dest):
+            print(f"✅ {weight_str} exists in {dest}")
+            return
+        WeightsDownloader.download(weight_str, url, dest)
+
+    @staticmethod
+    def download(weight_str, url, dest):
+        if "/" in weight_str:
+            subfolder = weight_str.rsplit("/", 1)[0]
+            dest = os.path.join(dest, subfolder)
+            os.makedirs(dest, exist_ok=True)
+
+        print(f"⏳ Downloading {weight_str} to {dest}")
+        start = time.time()
+        subprocess.check_call(
+            ["pget", "--log-level", "warn", "-xf", url, dest], close_fds=False
+        )
+        elapsed_time = time.time() - start
+        try:
+            file_size_bytes = os.path.getsize(
+                os.path.join(dest, os.path.basename(weight_str))
+            )
+            file_size_megabytes = file_size_bytes / (1024 * 1024)
+            print(
+                f"✅ {weight_str} downloaded to {dest} in {elapsed_time:.2f}s, size: {file_size_megabytes:.2f}MB"
+            )
+        except FileNotFoundError:
+            print(f"✅ {weight_str} downloaded to {dest} in {elapsed_time:.2f}s")

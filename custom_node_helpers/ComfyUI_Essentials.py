3f3d468511882978c36304aff50eb178eb88822b
Paul <fofr@users.noreply.github.com>
2024-07-08 09:49:06 +0000

    Remove unused helpers

--- custom_node_helpers/ComfyUI_Essentials.py
+++
@@ -1,8 +0,0 @@
-from custom_node_helper import CustomNodeHelper
-
-
-class ComfyUI_Essentials(CustomNodeHelper):
-    @staticmethod
-    def add_weights(weights_to_download, node):
-        if node.is_type("LoadCLIPSegModels"):
-            weights_to_download.extend(["models--CIDAS--clipseg-rd64-refined"])
